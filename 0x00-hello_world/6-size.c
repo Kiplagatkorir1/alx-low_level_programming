5e972818b4031cf418513aea338c0ce0c5a6aacc
Kiplagatkorir1 <kiplagat454@gmail.com>
2023-06-15 15:29:55 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,23 @@
 #include <stdio.h> 
   
- /** 
-  * main - Prints the size of various types 
-  * 
-  * Return: Always 0 (Success) 
-  */ 
+/** 
+* main - Prints the size of various types 
+* 
+* Return: Always 0 (Success) 
+*/ 
   
- int main(void) 
- {
-	 char a;
-         int b;
-         long int c;
-         long long d;
-         float e;
+int main(void) 
+{
+char a; 
+int b;
+long int c;
+long long d;
+float e;
 
-         printf("Size of a char: %d byte(s)\n", sizeof(a)); 
-         printf("Size of an int: %d byte(s)\n", sizeof(b)); 
-         printf("Size of a long int: %d byte(s)\n", sizeof(c)); 
-         printf("Size of a long long int: %d byte(s)\n", sizeof(d)); 
-         printf("Size of a float: %d byte(s)\n", sizeof(e)); 
-         return (0); 
- }
+printf("Size of a char: %d byte(s)\n", sizeof(a));  
+printf("Size of an int: %d byte(s)\n", sizeof(b)); 
+printf("Size of a long int: %d byte(s)\n", sizeof(c)); 
+printf("Size of a long long int: %d byte(s)\n", sizeof(d)); 
+printf("Size of a float: %d byte(s)\n", sizeof(e)); 
+return (0); 
+}
