a7383c4f4922c52a01e8e2c1f6e579d24c7b241f
Kiplagatkorir1 <kiplagat454@gmail.com>
2023-06-15 14:42:50 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("Size of a char:%zu btye(s)\n", sizeof(char));
+	printf("Size of a int:%zu btye(s)\n", sizeof(int);
+	printf("Size of a long int:%zu btye(s)\n", sizeof(long int);
+	printf("Size of a long long int:%zu btye(s)\n", sizeof(long long int);
+	printf("Size of a float:%zu btye(s)\n", sizeof(float);
+	return (0);
