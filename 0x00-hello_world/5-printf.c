af40e485f42fd2bfc5facdac7308109bfb233539
Kiplagatkorir1 <kiplagat454@gmail.com>
2023-06-15 14:31:27 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - A program that print a luine using printf function
- * return: 0(Success)
+ * Return: 0(Success)
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 		return (0);
 }
